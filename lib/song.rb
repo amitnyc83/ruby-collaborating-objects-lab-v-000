f781e402ef736870c3ffbf34c07288518f0acda6
Amit Singh <amit_nyc@live.com>
2018-07-03 17:12:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,13 +8,10 @@ def initialize(name)
   @name = name
 end
 
-def artist_name(name)
-  if (self.artist.nil?)
+def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-  else
-    self.artist.name = name
+    artist.add_song(self)
   end
-end
 
 
 
