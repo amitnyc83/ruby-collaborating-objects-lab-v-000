3ef1f24a70c09ddff57aa749a8f93472fb70ffd9
Amit Singh <amit_nyc@live.com>
2018-07-03 17:12:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,12 +17,13 @@ def artist_name(name)
 end
 
 
-def self.new_by_filename(filename)
-  song_name = filename.split(" - ")[1]
-  new_song = self.new(song_name)
-  new_song.artist_name = filename.split(" - ")[0]
-  new_song
-end
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
 
 
 
